5f4070ec362cb0f3ce6deadbd8cfd8515160bf35
Volodymyr <vthehang@gmail.com>
2022-08-26 17:10:19 +0300

    added base web server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("hello world!");
+import Fastify from "fastify";
+import dotenv from "dotenv";
+dotenv.config();
+
+const server = Fastify();
+const PORT = Number(process.env.PORT) || 5000;
+
+async function main() {
+    try {
+        await server.listen({
+            port: PORT,
+            host: "0.0.0.0",
+        });
+        console.log(`Server started on port ${PORT}`);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+main();
