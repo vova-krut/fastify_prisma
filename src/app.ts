b7d0e1f6b99a186f180c8fcc9b54146575fdaf51
Volodymyr <vthehang@gmail.com>
2022-08-28 16:03:56 +0300

    added swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import userRoutes from "./modules/user/user.routes";
 import { userSchemas } from "./modules/user/user.schema";
 import { productSchemas } from "./modules/product/product.schema";
 import fjwt from "@fastify/jwt";
-import dotenv from "dotenv";
+import swagger from "@fastify/swagger";
+import { withRefResolver } from "fastify-zod";
+import { version } from "../package.json";
 import productRoutes from "./modules/product/product.routes";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const server = Fastify();
@@ -29,6 +32,22 @@ declare module "@fastify/jwt" {
 server.register(fjwt, {
     secret: "d3k21n3k21n3j12neoiwqnidsadas1j12i31",
 });
+
+server.register(
+    swagger,
+    withRefResolver({
+        routePrefix: "/docs",
+        exposeRoute: true,
+        staticCSP: true,
+        openapi: {
+            info: {
+                title: "Fastify API",
+                description: "API for some products",
+                version,
+            },
+        },
+    })
+);
 
 server.decorate(
     "authenticate",
